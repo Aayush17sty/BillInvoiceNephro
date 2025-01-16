9d6674d9bc0c80e19440aa25f02d0f092a84533f
Aayush17sty <155305498+Aayush17sty@users.noreply.github.com>
2025-01-16 22:08:59 +0530

    Update settings.py

--- demo/settings.py
+++ demo/settings.py
@@ -123,7 +123,8 @@ CSRF_TRUSTED_ORIGINS = ['https://billinvoicenephro-production.up.railway.app']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
